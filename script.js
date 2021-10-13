e9cfcaf8c915916843402f9a168a72a57729eeee
Emin Koroglu <eminnkoroglu.1@gmail.com>
2021-10-13 13:48:09 -0500

    completed project

--- script.js
+++ script.js
@@ -6,90 +6,102 @@ const fieldCharacter = '░';
 const pathCharacter = '*';
 
 class Field {
-    constructor(arr, height, width){
-      this.field = arr;
-      this.gameWon = false;
-      this.height = height;
-      this.width = width;
+    constructor(field = [[]]){
+      this.field = field;
+      this.width = 0;
+      this.height = 0;
+      this.field[0][0] = pathCharacter;
     }
 
-    getInput() {
-      while(!this.gameWon){
-        let moveDir = prompt("Which way? ");
-        moveDir.toLowerCase;
-        for(let i = 0; i < this.height; i++){
-          for(let x = 0; x < this.width; x++){
-            if(moveDir === "r"){
-              //move to the right (increase width) (if cant move to the right, output error)
-            } else if(moveDir === "l"){
-              //move to the left (decrease width) (if cant move to the left, output error)
-            } else if(moveDir === "d"){
-              //move down (increase height) (if cant move down, output error)
-            } else if(moveDir === "u"){
-              //move up (decrease height) (if cant move up, output error)
-            }
-          }
+    runGame() {
+      let playing = true;
+      while(playing) {
+        this.print();
+        this.askQuestion();
+        if(!this.inBounds()) {
+          console.log("You are out of bounds!");
+          playing = false;
+          break;
+        } else if(this.isOnHole()) {
+          console.log("You are on a hole!");
+          playing = false;
+          break;
+        } else if(this.isOnHat()) {
+          console.log("Congratulations! You have won.");
+          playing = false;
+          break;
         }
-      }
-    }
-
-    getLocation() {
-      console.log(this.field.length)
-      for(let i = 0; i < this.field.length; i++){
-        for(let k = 0; k <= 5; k++){
-          console.log(this.field[i][k]);
-        }
+        this.field[this.height][this.width] = pathCharacter;
       }
     }
 
     print() {
-      this.field = this.field.map(e => e.join("")).join("");
-      console.log(this.field);
-      for(let i = 0; i < this.field.length; i++){
-        if(i % 5 === 0) {
-          //add a \n in every 5 i's
-        }
+      const displayField = this.field.map(row => {
+        return row.join("")
+      }).join("\n");
+      console.log(displayField);
+    }
+
+    askQuestion() {
+      let direction = prompt("Which way? ").toLowerCase();
+      switch(direction) {
+        case 'r':
+          this.width += 1;
+          break;
+        case 'l' :
+          this.width -= 1;
+          break;
+        case 'u' :
+          this.height -= 1;
+          break;
+        case 'd' :
+          this.height += 1;
+          break;
+        default:
+          console.log("Enter u, d, r, or l");
+          this.askQuestion();
+          break;
       }
     }
 
-    static generateField(height, width) { //generates a random field for user to play on
-      //generates a blank 2d Array
-      this.height = height;
-      this.width = width;
-      let array = new Array(height); 
-      for(let m = 0; m < height; m++) {
-        array[m] = new Array(width);
-      }
-      array[0][0] = pathCharacter; //sets the player at topleft position
-      //iterates through 2d array to randomly place field and holes
-      for(let i = 0; i < height; i++) {
-        for(let j = 0; j < width; j++) {
-          if(i === 0 && j === 0) { //dont place field or hole in leftmost position
-            continue;
-          }
-          let randomNum = Math.floor(Math.random() * 3)
-          if(randomNum === 2) {
-            array[i][j] = hole;
-          } else {
-            array[i][j] = fieldCharacter;
-          }
+    inBounds() {
+      return (
+        this.width >= 0 && this.height >= 0 &&
+        this.height < this.field.length &&
+        this.width < this.field[0].length
+      );
+    }
+
+    isOnHole() {
+      return this.field[this.height][this.width] === hole;
+    }
+
+    isOnHat() {
+      return this.field[this.height][this.width] === hat;
+    }
+
+    static generateField(height, width, probability = 0.1) {
+      const field = new Array(height).fill(0).map(e => new Array(width));
+      for(let y = 0; y < height; y++) {
+        for(let x = 0; x < width; x++) {
+          const prob = Math.random();
+          field[y][x] = prob > probability ? fieldCharacter : hole;
         }
       }
-      this.field = array;
+
+      const hatLocation = {
+        x: Math.floor(Math.random() * width),
+        y: Math.floor(Math.random() * height)
+      }
+
+      while(hatLocation.x === 0 && hatLocation.y === 0) {
+        hatLocation.x = Math.floor(Math.random() * width);
+        hatLocation.y = Math.floor(Math.random() * height);
+      }
+      field[hatLocation.y][hatLocation.x] = hat;
+      return field;
     }
-  }
+}
 
-const myField = new Field([
-    [pathCharacter, fieldCharacter, fieldCharacter, fieldCharacter, fieldCharacter],
-    [fieldCharacter, fieldCharacter, fieldCharacter, hole, fieldCharacter],
-    [fieldCharacter, hole, fieldCharacter, fieldCharacter, fieldCharacter],
-    [hole, fieldCharacter, hole, fieldCharacter, fieldCharacter],
-    [fieldCharacter, hole, hole, fieldCharacter, hole],
-    [hole, fieldCharacter, fieldCharacter, fieldCharacter, fieldCharacter],
-    [hole, hole, fieldCharacter, fieldCharacter, hole],
-    [fieldCharacter, fieldCharacter, fieldCharacter, fieldCharacter, hole],
-    [fieldCharacter, fieldCharacter, fieldCharacter, hole, hole],
-    [hat, hole, hole, fieldCharacter, hole],
-], 10, 5)
-
-myField.print();
+const myField = new Field(Field.generateField(10, 10));
+myField.runGame();
